ad786dce9025bffed8b1d11692838a29e6e1489d
John <john@Quentins-MacBook-Air.local>
2016-06-10 14:33:47 -0400

    added help text to edge_weight.sh

--- edge_weight.sh
+++ edge_weight.sh
@@ -6,7 +6,13 @@
 # $4 is the path to the desired output directory
 # $5 is the volume of each voxel
 # $6 is the number of seeds per voxel
-# $7 is a list of nodes of interest in the format "first second third etc."
+# $7 is a list of nodes of interest in the format "first,second,third,etc"
+
+#help text
+if [ "$1" == "-h" ]; then
+  printf "Usage: \n"\$1" [path to edge_weight.R] \n"\$2" [path to track_network output] \n"\$3" [path to surface area output] \n"\$4" [path to desired output directory] \n"\$5" [volume of each voxel] \n"\$6" [number of seeds per voxel] \n"\$7" [optional argument that contains nodes of interest. Defaults to outputting edge weights between all nodes.]"
+  exit 0
+fi
 
 R="$1"
 TRK="$2"
@@ -15,7 +21,5 @@ OUT="$4"
 VOL="$5"
 SEED="$6"
 LIST="$7"
-
-echo "$LIST"
 
 Rscript $R --args $TRK $SA $OUT $VOL $SEED $LIST
